3903396007f6379005fbc4d96269bde64de56651
Ahmed Shaaban <ahmed.vuw@gmail.com>
2016-01-29 23:28:49 +1300

    Added script to test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+public float MoveSpeed = 10;
+public float RotateSpeed = 40;
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float MoveForward = Input.GetAxis("Vertical");
+    	float MoveRotate = Input.GetAxis("Horizontal");
+
+		// Move the player
+ 		transform.Translate(Vector3.forward * MoveForward);
+ 		transform.Rotate(Vector3.up * MoveRotate);
+
+		
 	}
 }
