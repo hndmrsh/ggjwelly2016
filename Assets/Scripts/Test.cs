e7838c43072919585eda08706c30c40c0fc7f59c
danabrandala <danabrandala@gmail.com>
2016-01-29 22:43:48 +1300

    Random prefab location

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,21 +3,21 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
-
-	public Vector3 cube2;
-
+	public GameObject prefab;
 
 	// Use this for initialization
 	void Start () {
 	
-
-
-
+		Vector3 position = new Vector3(Random.Range(1f, 10f), 0.5f, Random.Range(1f, 10f));
+		Instantiate(prefab, position, Quaternion.identity);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
+
 	
 	}
 }
