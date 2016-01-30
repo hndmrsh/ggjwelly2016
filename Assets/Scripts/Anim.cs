fb3650231f715955f497082091fe9ab6f85b95cd
danabrandala <danabrandala@gmail.com>
2016-01-30 21:32:23 +1300

    Scene2
    BoxPeople

---
+++ Assets/Scripts/Anim.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim : MonoBehaviour {
+
+	public bool MouseClicked;
+
+	public Animator animation;	
+
+
+	// Use this for initialization
+	void Start () {
+
+		MouseClicked = false;
+		animation = GetComponent<Animator>();
+
+	
+	}
+
+
+	void Update(){
+		
+		if(Input.GetMouseButtonDown(0))
+		{
+
+			Debug.Log ("MOUSE CLICK");
+			animation.SetBool("MouseClicked", true); 
+		}
+
+		//if got to target position//
+		if(Input.GetMouseButtonDown(1))
+		{
+			animation.SetBool("MouseClicked", false);
+			}
+
+	}
+	
+
+}
